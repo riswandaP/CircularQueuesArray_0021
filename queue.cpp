be937011e33aecdbf475d66a49158b8ff88a6746
riswandaP <riswandaputra03@gmail.com>
2025-07-09 13:54:50 +0700

    mengecek apakah antrian kosong

--- queue.cpp
+++ queue.cpp
@@ -24,4 +24,10 @@ class Queues
         cin >> num;
         cout << endl;
         // Cek apakah antrian Penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+        // Cek apakah antrian Kosong
  
