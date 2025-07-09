1df58acbfdebcb03de79cb341297032c86f03c9b
riswandaP <riswandaputra03@gmail.com>
2025-07-09 13:56:33 +0700

    membuat void Remove

--- queue.cpp
+++ queue.cpp
@@ -38,4 +38,11 @@ class Queues
         else
         {
             // Jika REAR berada di posisi terakhir array, kembali ke awal array
- 
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+    void Remove()
